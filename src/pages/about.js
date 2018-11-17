fe06bc99ba57c53e71fc6c8f19da7479b56172fa
Karam Malkon <karammalkon@Karams-MacBook-Pro.local>
2018-11-17 12:25:34 +0100

    added About excerpt and contact links

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,9 @@ import { StaticQuery, graphql } from 'gatsby'
 import { css } from 'react-emotion'
 
 const pageTitleContainerClass = css`
-    display: flex;
-    justify-content: center;
-`;
+  display: flex;
+  justify-content: center;
+`
 const pageTitleClass = css`
   color: #cc5f5f;
   text-transform: uppercase;
@@ -15,10 +15,28 @@ const pageTitleClass = css`
 
 export default ({ data }) => (
   <Layout>
-    <div className={pageTitleContainerClass} >
+    <div className={pageTitleContainerClass}>
       <h1 className={pageTitleClass}>About</h1>
     </div>
-    <div>This is the about page, is that okay mate?</div>
+     {' '}
+    <div>
+      <p>
+        I’m a frontend developer currently living in Stockholm, Sweden.
+        My favorite technologies right now are React, GraphQL and Gatsby. I also
+        have worked with Ruby on Rails, AngularJS, PHP, CSS, SASS and LESS.{' '}
+      </p>
+      <p>
+        When I'm not working, I like to unleash my inner kid by playing video
+        games (mostly fantasy card games), taking candid pictures of people in
+        the streets (AKA street photography), experiencing different kinds of
+        food, reading and spending time with my girlfriend.
+      </p>
+      <p>
+        If you would like to talk about any of the subjects I mentioned (my
+        girlfriend excluded) or just have a curious question, feel free to say
+        hello and share your stories with me!
+      </p>
+    </div>
   </Layout>
 )
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,21 +4,57 @@ import { StaticQuery, graphql } from 'gatsby'
 import { css } from 'react-emotion'
 
 const pageTitleContainerClass = css`
-    display: flex;
-    justify-content: center;
-`;
+  display: flex;
+  justify-content: center;
+`
 const pageTitleClass = css`
   color: #cc5f5f;
   text-transform: uppercase;
   font-size: 5rem;
 `
+const contactWrapperClass = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  ${'' /* grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); */}
+  color: #444;
+  text-align: center;
+  font-size: 4rem;
+  grid-gap: 50px;
+  height: 50vh;
+  margin-top: 60px;
+`
+
+const contactLinksClass = css`
+  text-decoration: none;
+  color: inherit;
+`
 
 export default ({ data }) => (
   <Layout>
-    <div className={pageTitleContainerClass} >
+    <div className={pageTitleContainerClass}>
       <h1 className={pageTitleClass}>Contact</h1>
     </div>
-    <div>This is the contact page, is that okay mate?</div>
+
+    <div className={contactWrapperClass}>
+      <div>
+        <a href="https://github.com/kmelkon" className={contactLinksClass}>GitHub</a>
+      </div>
+      <div>
+        <a href="https://twitter.com/kmelkon" className={contactLinksClass}>Twitter</a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/kmelkon/" className={contactLinksClass}>Linkedin</a>
+      </div>
+      <div>
+        <a href="https://dev.to/kmelkon" className={contactLinksClass}>DEV</a>
+      </div>
+      <div>
+        <a href="https://www.instagram.com/kmelkon/" className={contactLinksClass}>Instagram</a>
+      </div>
+      <div>
+        <a href="mailto:kmelkon@gmail.com" className={contactLinksClass}>Email</a>
+      </div>
+    </div>
   </Layout>
 )
 
