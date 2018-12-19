342580bd9a2e211d0628518f1a2eca01076457c4
Karam <kmelkon@gmail.com>
2018-12-19 14:50:33 +0100

    💄 Added a project and replaced flex with grid

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,12 +4,53 @@ import { Link } from 'gatsby'
 import containerStyles from './projects.module.scss'
 import { FiGithub } from 'react-icons/fi'
 
-export default ({ data }) => (
+export default () => (
   <Layout>
     <div className={containerStyles.titleContainer}>
       <h1 className={containerStyles.title}>Projects</h1>
     </div>
     <div className={containerStyles.projects}>
+      <div className={containerStyles.project}>
+        <h3 className={containerStyles.projectTitle}>React Clock</h3>
+        <p>
+          A simple react clock that displays the time in both 24 and 12 hour
+          formats.
+        </p>
+        <div className={containerStyles.linkWrapper}>
+          <a
+            href="https://github.com/kmelkon/react-clock"
+            className={containerStyles.link}
+          >
+            <FiGithub />
+          </a>
+          <a
+            href="https://admiring-cori-0b0a58.netlify.com/"
+            className={containerStyles.link}
+          >
+            Go to project
+          </a>
+        </div>
+      </div>
+
+      <div className={containerStyles.project}>
+        <h3 className={containerStyles.projectTitle}>React Clicker</h3>
+        <p>A simple react clicker/incremental game for fun and profit.</p>
+        <div className={containerStyles.linkWrapper}>
+          <a
+            href="https://github.com/kmelkon/react-clicker"
+            className={containerStyles.link}
+          >
+            <FiGithub />
+          </a>
+          <a
+            href="https://romantic-knuth-651f53.netlify.com/"
+            className={containerStyles.link}
+          >
+            Go to project
+          </a>
+        </div>
+      </div>
+
       <div className={containerStyles.project}>
         <h3 className={containerStyles.projectTitle}>
           Karam Malkon's Portfolio/Blog
@@ -25,24 +66,6 @@ export default ({ data }) => (
           <Link to="/" className={containerStyles.link}>
             Go to project
           </Link>
-        </div>
-      </div>
-      <div className={containerStyles.project}>
-        <h3 className={containerStyles.projectTitle}>React Clicker</h3>
-        <p>A simple react clicker/incremental game.</p>
-        <div className={containerStyles.linkWrapper}>
-          <a
-            href="https://github.com/kmelkon/react-clicker"
-            className={containerStyles.link}
-          >
-            <FiGithub />
-          </a>
-          <a
-            href="https://romantic-knuth-651f53.netlify.com/"
-            className={containerStyles.link}
-          >
-            Go to project
-          </a>
         </div>
       </div>
     </div>
